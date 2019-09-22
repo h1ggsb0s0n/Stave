8d515068d1c770c8849e5f4cae79fcd1d6750fcf
h1ggsb0s0n <nic.haefeli@gmail.com>
2019-09-22 17:19:35 +0200

    new project

---
+++ canvas.js
@@ -0,0 +1,307 @@
+var canvas = document.querySelector("canvas");
+
+canvas.width = 1000;
+canvas.height = 1000;
+var c =  canvas.getContext("2d");
+
+var mouse = {
+  x: undefined,
+  y: undefined
+}
+
+var trebleClefImage = new Image();
+trebleClefImage.src = 'media/clef.png';
+var bassClefImage = new Image();
+bassClefImage.src = 'media/bassclef.png';
+
+canvas.addEventListener("mousemove", function(event){
+  mouse.x = event.x;
+  mouse.y = event.y;
+});
+
+
+
+function drawLine(x,y,x2){
+  c.beginPath();
+  c.moveTo(x,y)
+  c.lineTo(x2,y);
+  c.stroke();
+}
+
+function Note(radius){
+
+  this.radius = radius;
+
+  this.draw = function(x,y,color){
+    c.beginPath();
+    c.arc(x, y, this.radius, 0, Math.PI * 2, false); // x, y, radius  ,startAngle, endAngle, drawcounterclockwise
+    c.strokeStyle = color;
+    c.fillStyle = color; //selects any color in the array
+    c.stroke();
+    c.fill();
+  }
+}
+
+
+function HelpingLine(x,y,length){
+  this.length = length();
+  c.beginPath(x,y);
+  c.beginPath()
+  c.stroke();
+}
+
+//add color of lines maybe?
+//add y position where to draw notes
+//adding of new parameter, number of HelpLines
+function Stave(x,y,length,gap){
+  this.x = x;
+  this.y = y;
+  this.yTrebleFirstHelpLine = this.y - gap*5;
+  this.yTrebleLastHelpLine = this.y + gap* 13;
+  this.yBassFirstHelpLine = this.y + gap*15;
+  this.yBassLastHelpLine = this.y + gap*33;
+  this.trebleNotes = ["D","C","H","A","G","F","E","D","C","H","A","G","F","E","D","C","H","A","G"];
+  this.bassNotes =  ["F","E","D","C","H","A","G","F","E","D","C","H","A","G","F","E","D","C","H"];
+  this.gap = gap;
+  this.note = new Note(gap/2);
+  this.currentLineNumber;
+  this.length = length;
+  this.addedNotes = [];
+
+  this.draw = function(){
+    //draw treble Lines
+    drawLine(this.x,this.y,this.x+this.length);
+    drawLine(this.x,this.y+this.gap,this.x+this.length);
+    drawLine(this.x,this.y+(this.gap*2),this.x+this.length);
+    drawLine(this.x,this.y+(this.gap*3),this.x+this.length);
+    drawLine(this.x,this.y+(this.gap*4),this.x+this.length);
+
+    //draw Bass Lines
+    drawLine(this.x,this.y+(this.gap*10),this.x+this.length);
+    drawLine(this.x,this.y+(this.gap*11),this.x+this.length);
+    drawLine(this.x,this.y+(this.gap*12),this.x+this.length);
+    drawLine(this.x,this.y+(this.gap*13),this.x+this.length);
+    drawLine(this.x,this.y+(this.gap*14),this.x+this.length);
+
+  }
+
+//problem we do have more than 10
+  this.addNoteToStave = function(){
+    this.addedNotes.push(this.currentLineNumber);
+    return this.currentLineNumber;
+  }
+
+  this.returnSelectedNote = function(){
+    if(this.currentLineNumber < 15 ){
+      return this.trebleNotes[this.currentLineNumber + 5];
+    } else{
+      return this.bassNotes[this.currentLineNumber - 15];
+    }
+  }
+  //checks if notes have been added to the stav
+  this.containsNotes = function(){
+    if(addedNotes.length > 0){
+      return true;
+    } else{
+      return false;
+    }
+  }
+
+
+  this.drawNote = function(x,y,color){
+    this.note.draw(x+(this.length/2),y,color);
+  }
+  //checks on which line (+- gap between the lines/4) the MousePointer is located.
+  this.isOnLine = function(lineNumber){
+    var linePosition = this.y + (lineNumber * (this.gap/2));
+    if(mouse.y >= (linePosition) - (this.gap/4) && mouse.y <= (linePosition) + (this.gap/4)){
+      return true;
+    } return false;
+  }
+
+  //returns the closestLine relative to the mouseposition
+  this.closestLine = function(){
+    var line = this.yTrebleFirstHelpLine;
+    for (var i = 0; i < 33; i++) {
+      console.log("MouseY "+mouse.y);
+      console.log("i = " + i);
+      if(mouse.y < y){
+        return i--;
+      }
+      line = line + this.gap;
+    }
+  }
+
+  //returns the y position of a selected line
+  this.returnLinePosition = function(lineNumber){
+    return this.y + (lineNumber * (this.gap/2));
+  }
+
+  this.drawOnLine = function(lineNumber){
+    var yLinePosition = this.y + (lineNumber * (this.gap/2));
+    this.note.draw(this.x +(this.length/2), yLinePosition);
+    if(linenumber = -1 || lineNumber)
+    this.currentLineNumber = lineNumber;
+
+  }
+
+  this.drawHelpLine = function(lineNumber){
+    var yLinePosition = this.y + (lineNumber * (this.gap/2));
+    //drawLine(x,y,x2)
+    drawLine(this.x +(this.length/2)-30, yLinePosition, this.x +(this.length/2)+30);
+  }
+
+  this.drawAddedNotes = function(){
+    this.addedNotes.forEach(function(lineNumber){
+      this.drawOnLine(lineNumber);
+      /*if(lineNumber < 0 || (lineNumber > 9 && lineNumber < 19) || lineNumber > 29){
+        this.drawHelpLine(lineNumber);
+      }*/
+
+    }, this);
+  }
+
+
+  //this.note.draw(this.x + (this.length/2),this.y);
+  //umstellung -> return line Positionthis.y
+  this.update = function(){
+
+    //treble
+    if(this.isOnLine(-5)){
+      this.drawOnLine(-5);
+      this.drawHelpLine(-4);
+      this.drawHelpLine(-2);
+    }
+    if(this.isOnLine(-4)){
+      this.drawOnLine(-4);
+      this.drawHelpLine(-4);
+      this.drawHelpLine(-2);
+    }
+    if(this.isOnLine(-3)){
+      this.drawOnLine(-3);
+      this.drawHelpLine(-2);
+    }
+    if(this.isOnLine(-2)){
+      this.drawOnLine(-2);
+      this.drawHelpLine(-2);
+    }
+    if(this.isOnLine(-1)){
+      this.drawOnLine(-1);
+    }
+    if(this.isOnLine(0)){
+      this.drawOnLine(0);
+    } else if(this.isOnLine(1)){
+      this.drawOnLine(1);
+    } else if(this.isOnLine(2)){
+      this.drawOnLine(2);
+    } else if(this.isOnLine(3)){
+      this.drawOnLine(3);
+    } else if(this.isOnLine(4)){
+      this.drawOnLine(4);
+    } else if(this.isOnLine(5)){
+      this.drawOnLine(5);
+    } else if(this.isOnLine(6)){
+      this.drawOnLine(6);
+    }else if(this.isOnLine(7)){
+      this.drawOnLine(7);
+    } else if(this.isOnLine(8)){
+      this.drawOnLine(8);
+    }else if(this.isOnLine(9)){
+      this.drawOnLine(9);
+    }else if(this.isOnLine(10)){
+      this.drawOnLine(10);
+      this.drawHelpLine(10);
+    }else if(this.isOnLine(11)){
+      this.drawOnLine(11);
+      this.drawHelpLine(10);
+    }else if(this.isOnLine(12)){
+      this.drawOnLine(12);
+      this.drawHelpLine(10);
+      this.drawHelpLine(12);
+    }else if(this.isOnLine(13)){
+      this.drawOnLine(13);
+      this.drawHelpLine(10);
+      this.drawHelpLine(12);
+    }
+
+    //bass
+    else if(this.isOnLine(15)){
+      this.drawOnLine(15);
+      this.drawHelpLine(16);
+      this.drawHelpLine(18);
+    }
+    else if(this.isOnLine(16)){
+      this.drawOnLine(16);
+      this.drawHelpLine(16);
+      this.drawHelpLine(18);
+    }
+    else if(this.isOnLine(17)){
+      this.drawOnLine(17);
+      this.drawHelpLine(18);
+    }
+    else if(this.isOnLine(18)){
+      this.drawOnLine(18);
+      this.drawHelpLine(18);
+    }
+    else if(this.isOnLine(19)){
+      this.drawOnLine(19);
+    }else if(this.isOnLine(20)){
+      this.drawOnLine(20);
+    } else if(this.isOnLine(21)){
+      this.drawOnLine(21);
+    } else if(this.isOnLine(22)){
+      this.drawOnLine(22);
+    } else if(this.isOnLine(23)){
+      this.drawOnLine(23);
+    } else if(this.isOnLine(24)){
+      this.drawOnLine(24);
+    } else if(this.isOnLine(25)){
+      this.drawOnLine(25);
+    }else if(this.isOnLine(26)){
+      this.drawOnLine(26);
+    } else if(this.isOnLine(27)){
+      this.drawOnLine(28);
+    }else if(this.isOnLine(29)){
+      this.drawOnLine(29);
+    }else if(this.isOnLine(30)){
+      this.drawOnLine(30);
+      this.drawHelpLine(30);
+    }else if(this.isOnLine(31)){
+      this.drawOnLine(30);
+      this.drawHelpLine(30);
+    }else if(this.isOnLine(32)){
+      this.drawOnLine(32);
+      this.drawHelpLine(30);
+      this.drawHelpLine(32);
+    }else if(this.isOnLine(33)){
+      this.drawOnLine(33);
+      this.drawHelpLine(30);
+      this.drawHelpLine(32);
+    }
+
+    else {
+      this.draw();
+    }
+  }
+}
+
+var stave = new Stave(50,71,700,20);
+function animate() {
+
+  requestAnimationFrame(animate); //creates a loop for us -> Function Animate
+  c.clearRect(0, 0, innerWidth, innerHeight);
+  stave.draw();
+  stave.drawAddedNotes();
+  stave.update();
+  //achtung hier möchte er auch hier immer die linien grau machen.
+  c.drawImage(trebleClefImage, 20,20);
+  c.drawImage(bassClefImage, 40, 243, 130, 130);
+}
+
+canvas.addEventListener("click", function(event){
+  stave.addNoteToStave();
+  console.log(stave.closestLine());
+  console.log(stave.returnSelectedNote());
+});
+
+animate();
---
+++ trebleClef.js
@@ -0,0 +1,100 @@
+
+          var canvas = document.getElementById('canvas');
+          if (canvas.getContext){
+              var ctx = canvas.getContext('2d');
+
+              ctx.beginPath();
+              ctx.moveTo(159,3);
+              ctx.quadraticCurveTo(129,50,117,93);
+              ctx.quadraticCurveTo(107,126,102,167);
+              ctx.quadraticCurveTo(101,192,102,210);
+              ctx.quadraticCurveTo(107,255,116,297);
+              ctx.quadraticCurveTo(63,351,44,375);
+              ctx.quadraticCurveTo(24,401,15,429);
+              ctx.quadraticCurveTo(2,464,3,503);
+              ctx.quadraticCurveTo(5,540,20,575);
+              ctx.quadraticCurveTo(29,596,48,615);
+              ctx.quadraticCurveTo(62,630,87,645);
+              ctx.quadraticCurveTo(113,660,150,666);
+              ctx.quadraticCurveTo(177,668,194,665);
+              ctx.quadraticCurveTo(204,720,213,776);
+              ctx.quadraticCurveTo(216,795,216,813);
+              ctx.quadraticCurveTo(203,849,158,857);
+              ctx.quadraticCurveTo(132,857,120,842);
+              ctx.quadraticCurveTo(152,845,166,813);
+              ctx.quadraticCurveTo(165,821,168,802);
+              ctx.quadraticCurveTo(166,775,151,765);
+              ctx.quadraticCurveTo(132,750,107,758);
+              ctx.quadraticCurveTo(86,768,78,789);
+              ctx.quadraticCurveTo(71,818,90,840);
+              ctx.quadraticCurveTo(105,857,129,865);
+              ctx.quadraticCurveTo(149,872,177,865);
+              ctx.quadraticCurveTo(194,860,209,846);
+              ctx.quadraticCurveTo(231,828,230,803);
+              ctx.quadraticCurveTo(221,735,207,662);
+              ctx.quadraticCurveTo(248,650,267,626);
+              ctx.quadraticCurveTo(293,599,296,566);
+              ctx.quadraticCurveTo(300,527,285,494);
+              ctx.quadraticCurveTo(270,462,234,444);
+              ctx.quadraticCurveTo(215,435,189,435);
+              ctx.quadraticCurveTo(177,435,164,438);
+              ctx.quadraticCurveTo(155,396,146,354);
+              ctx.quadraticCurveTo(183,315,203,275);
+              ctx.quadraticCurveTo(219,243,222,210);
+              ctx.quadraticCurveTo(227,167,221,137);
+              ctx.quadraticCurveTo(213,93,192,51);
+              ctx.quadraticCurveTo(180,29,159,3);
+
+              ctx.fill();
+
+              ctx.fillStyle = "white";
+              ctx.beginPath();
+              ctx.moveTo(191,93);
+              ctx.quadraticCurveTo(179,83,171,93);
+              ctx.quadraticCurveTo(126,162,131,281);
+              ctx.quadraticCurveTo(188,239,203,188);
+              ctx.quadraticCurveTo(209,162,204,135);
+              ctx.quadraticCurveTo(200,111,191,93);
+              ctx.fill();
+
+              ctx.fillStyle = "white";
+              ctx.beginPath();
+              ctx.moveTo(171,473);
+              ctx.quadraticCurveTo(188,555,206,648);
+              ctx.quadraticCurveTo(237,639,255,620);
+              ctx.quadraticCurveTo(283,588,283,558);
+              ctx.quadraticCurveTo(285,525,269,501);
+              ctx.quadraticCurveTo(252,476,216,470);
+              ctx.quadraticCurveTo(194,465,171,473);
+
+              ctx.fill();
+
+              ctx.fillStyle = "white";
+              ctx.beginPath();
+              ctx.moveTo(147,446);
+              ctx.quadraticCurveTo(141,411,132,369);
+              ctx.quadraticCurveTo(90,401,68,435);
+              ctx.quadraticCurveTo(45,467,39,503);
+              ctx.quadraticCurveTo(30,540,45,576);
+              ctx.quadraticCurveTo(60,612,92,633);
+              ctx.quadraticCurveTo(123,651,161,654);
+              ctx.quadraticCurveTo(174,654,188,653);
+              ctx.fill();
+
+              ctx.fillStyle = "black";
+              ctx.beginPath();
+              ctx.moveTo(147,444);
+              ctx.quadraticCurveTo(120,456,101,480);
+              ctx.quadraticCurveTo(83,504,84,536);
+              ctx.quadraticCurveTo(86,567,107,588);
+              ctx.quadraticCurveTo(114,597,126,605);
+              ctx.quadraticCurveTo(116,593,107,581);
+              ctx.quadraticCurveTo(95,560,99,537);
+              ctx.quadraticCurveTo(105,509,132,491);
+              ctx.quadraticCurveTo(143,482,164,476);
+
+              ctx.fill();
+
+          } else {
+              alert('You need Firefox 1.5+, Google Chrome 8.0+, Internet Explorer 8.0+ or Safari 4.0+ to see the example.');
+          }
